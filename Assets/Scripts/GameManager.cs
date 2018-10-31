3c3dc14197459e0adb7e09047b9a0d0e3eff1299
Matt Payne <matthew.payne1988@gmail.com>
2018-10-31 16:27:04 +1300

    GameManager.cs update
    added camera color control but need to move this to the actual camera

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,40 +4,84 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public int seed;
+    [Header("Background Color Controls")]
+    public float colorChangeTime;
+    public float colorChangeMin = 5f;
+    public float colorChangeMax = 15f;
+    [Range(0f, 1f)]
+    public float colorChangeSpeed = 1f;
+    [Space]
+    public float minHue = 0f;
+    public float maxHue = 1f;
+    public float minSaturation = 0.6f;
+    public float maxSaturation = 0.9f;
+    public float minValue = 0.8f;
+    public float maxValue = 1f;
+    public float minAlpha = 1f;
+    public float maxAlpha = 1f;
 
-    public static int resizeSeed;
-    //public static List<GameObject> pucks;
-    public static List<GameObject> pucks;
 
+    public static Camera mainCam;
+    public static GameObject[] pucks;
+
+    private bool colorToggle;
+    private Color currentColor;
+    private Color newColor;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+        pucks = GameObject.FindGameObjectsWithTag("Puck");
+    }
 
     private void Start()
     {
-        pucks = new List<GameObject>();
-        GetPucks();
-
-        foreach (GameObject p in pucks)
-        {
-            print(p.name);
-        }
-
+        currentColor = NewRandColor();
+        StartCoroutine(TriggerBackgroundColorChange());
     }
 
     private void Update()
     {
-        seed = resizeSeed;
-        
+        UpdateBackgroundColor();
     }
 
-    void GetPucks()
+    void UpdateBackgroundColor()
     {
-        foreach(GameObject p in GameObject.FindGameObjectsWithTag("Puck"))
+        if (colorToggle)
         {
-            pucks.Add(p);
-        }  
-    }
-    
+            currentColor = Color.Lerp(currentColor, newColor, (colorChangeSpeed * colorChangeSpeed) * (Time.deltaTime / 2));
+        }
 
+        mainCam.backgroundColor = currentColor;
+    }
+
+    public Color NewRandColor()
+    {
+        return Random.ColorHSV(minHue, maxHue, minSaturation, maxSaturation, minValue, maxValue, minAlpha, maxAlpha);
+    }
+
+    IEnumerator TriggerBackgroundColorChange()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(colorChangeTime);
+
+            colorChangeTime = Random.Range(colorChangeMin, colorChangeMax);
+
+            if (colorToggle)
+            {
+                colorToggle = false;
+                currentColor = newColor;
+            }
+            else if (!colorToggle)
+            {
+                colorToggle = true;
+                newColor = NewRandColor();
+
+                Debug.Log("Changing Color");
+            }
+        }
+    }
 }
 
 
